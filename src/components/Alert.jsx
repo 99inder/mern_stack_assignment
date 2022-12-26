56f74e30d9493b1a8514d997e272830575c951db
Inderpal Singh <92572473+99inder@users.noreply.github.com>
2022-12-26 17:01:31 +0530

    Alert position fixed

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -5,7 +5,7 @@ export default function Alert(props) {
     return (
         <div className="my-2" style={{ height: '55px', textAlign: 'center'}}>
             {
-                props.alert && <div className={`alert alert-${props.alert.type}`} role="alert">
+                props.alert && <div className={`alert alert-fixed alert-${props.alert.type}`} role="alert">
                     <strong>{props.alert.type === 'danger'? "Error " : "Hurray! "}</strong> : {props.alert.message}.
                 </div>
             }
